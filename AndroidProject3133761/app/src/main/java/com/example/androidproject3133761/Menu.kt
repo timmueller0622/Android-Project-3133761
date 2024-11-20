a3fa4615f3082c30ee5c95e9f20b7267dbbcc985
timmu <112384574+timmueller0622@users.noreply.github.com>
2024-11-20 21:05:21 +0000

    add null check for uri in menu activity

--- AndroidProject3133761/app/src/main/java/com/example/androidproject3133761/Menu.kt
+++ AndroidProject3133761/app/src/main/java/com/example/androidproject3133761/Menu.kt
@@ -1,6 +1,5 @@
 package com.example.androidproject3133761
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.media.MediaPlayer
 import android.net.Uri
@@ -8,17 +7,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -26,25 +19,16 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.androidproject3133761.ui.theme.AndroidProject3133761Theme
 
 
 class Menu : ComponentActivity() {
@@ -58,10 +42,16 @@ class Menu : ComponentActivity() {
             var playSound: MediaPlayer = MediaPlayer() //initialization happens here with an empty player. sounds added for every button
             val context = this //context relevant to create mediaplayer and pass along in intent
 
-            val selectedAudioUri = Uri.parse(intent.getStringExtra("customSound"))
+            //add null check for uri
+            var selectedAudioUri = Uri.EMPTY
+            val uriFromIntent = intent.getStringExtra("customSound")
+            if (uriFromIntent == null){
 
-            val intent = Intent(this, Player::class.java)
-            Text(selectedAudioUri?.path.toString())
+            }
+            else
+                selectedAudioUri = Uri.parse(uriFromIntent)
+
+            val currentIntent = Intent(this, Player::class.java)
 
             Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
                 Text("Choose Your Sound")
@@ -100,7 +90,7 @@ class Menu : ComponentActivity() {
                     Spacer(modifier = Modifier.width(20.dp))
                     SoundButton(painterResource(id = R.drawable.music), selectedAudioUri?.path.toString(), {
                         playSound = MediaPlayer().apply {
-                            setDataSource(this@Menu, selectedAudioUri!!)
+                            //setDataSource(this@Menu, selectedAudioUri!!)
                             prepare()
                             start()
                         }
@@ -108,10 +98,10 @@ class Menu : ComponentActivity() {
                         soundId.value = selectedAudioUri?.path.toString()
                     })
                 }
-                // SoundButton(painterResource(id = R.drawable.music), "custom", {})
+                //SoundButton(painterResource(id = R.drawable.music), "custom", {})
                 ExtendedFloatingActionButton(onClick = {
-                    intent.putExtra("soundId", soundId.value)
-                    startActivity(intent)
+                    currentIntent.putExtra("soundId", soundId.value)
+                    startActivity(currentIntent)
                 }) {
                     Icon(Icons.Filled.PlayArrow, "play")
                     Text("Get Started")
